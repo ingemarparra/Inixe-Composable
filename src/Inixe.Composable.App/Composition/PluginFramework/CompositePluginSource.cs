942a67894479cd1075cddeb2b0acf1fb5db3cb96
ingemarparra <arsenico@inixe.com.mx>
2023-08-27 22:45:30 +0200

    Add plugin sources
    Plugin sources allow different abstractions for locating and providing
    plugins to the application.
    
    In this very first version, File based and composite sources are
    provided.
    
    The composite is a MUST since many sources can be attached and this
    implementation hides that fact also provides a yet simple way to extend
    and use other sources.
    
    The file based is aimed for json manifests. But there's still the
    possibility to detect other DLLs by inspecting their contents.

---
+++ src/Inixe.Composable.App/Composition/PluginFramework/CompositePluginSource.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CompositePluginSource.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition.PluginFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Inixe.Composable.UI.Core;
+
+    /// <summary>
+    /// Aggregator for plugin sources.
+    /// </summary>
+    /// <seealso cref="Inixe.Composable.UI.Core.IPluginSource" />
+    /// <remarks>This acts as a decorator that will eventually call any module registered <see cref="IPluginSource"/>.</remarks>
+    internal sealed class CompositePluginSource : IPluginSource
+    {
+        private readonly IList<IPluginSource> sources;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompositePluginSource"/> class.
+        /// </summary>
+        /// <param name="sources">The sources.</param>
+        public CompositePluginSource(IList<IPluginSource> sources)
+        {
+            this.sources = sources ?? (IList<IPluginSource>)new List<IPluginSource> { new NullPluginSource() };
+        }
+
+        /// <inheritdoc/>
+        public IList<IPluginManifest> FindManifests()
+        {
+            var allManifests = this.sources.SelectMany(x => x.FindManifests())
+                .ToList();
+
+            return allManifests;
+        }
+    }
+}
---
+++ src/Inixe.Composable.App/Composition/PluginFramework/FileSystemPluginSource.cs
@@ -0,0 +1,80 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="FileSystemPluginSource.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition.PluginFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Inixe.Composable.UI.Core;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Default file system plugin source. It will scan all plugin manifests from a directory.
+    /// </summary>
+    /// <seealso cref="Inixe.Composable.UI.Core.IPluginSource" />
+    internal class FileSystemPluginSource : IPluginSource
+    {
+        private readonly string sourceDirectoryPath;
+
+        private FileSystemPluginSource(string sourceDirectoryPath)
+        {
+            this.sourceDirectoryPath = sourceDirectoryPath;
+        }
+
+        public static IPluginSource Create(string sourceDirectoryPath)
+        {
+            if (string.IsNullOrWhiteSpace(sourceDirectoryPath))
+            {
+                throw new ArgumentException("Invalid source directory path", nameof(sourceDirectoryPath));
+            }
+
+            if (!Directory.Exists(sourceDirectoryPath))
+            {
+                throw new ArgumentException("Invalid path. The source directory must exist", nameof(sourceDirectoryPath));
+            }
+
+            return new FileSystemPluginSource(sourceDirectoryPath);
+        }
+
+        /// <inheritdoc/>
+        public IList<IPluginManifest> FindManifests()
+        {
+            const string SearchPattern = "*.manifest";
+
+            var files = Directory.EnumerateFiles(this.sourceDirectoryPath, SearchPattern, SearchOption.AllDirectories);
+
+            var manifests = files.Select(x => new ManifestFile(x, File.ReadAllText(x)))
+                .Select(LoadPluginManifestFromFile);
+
+            return manifests.ToList();
+        }
+
+        private static IPluginManifest LoadPluginManifestFromFile(ManifestFile manifestFile)
+        {
+            var manifest = JsonConvert.DeserializeObject<FileBasedPluginManifest>(manifestFile.Contents);
+            return manifest;
+        }
+
+        private record ManifestFile(string SourcePath, string Contents);
+
+        private class FileBasedPluginManifest : IPluginManifest
+        {
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+
+            public string SourcePath { get; set; }
+
+            public IReadOnlyDictionary<string, object> Properties { get; set; }
+
+            public string PluginFactoryTypeName { get; set; }
+
+            public string Description { get; set; }
+        }
+    }
+}
