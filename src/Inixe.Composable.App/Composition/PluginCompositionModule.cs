47f58d1697af3bbdda3d66a9b99c372204ceb83b
ingemarparra <arsenico@inixe.com.mx>
2023-08-27 23:14:38 +0200

    Add Plugin composition module
    This module brings and registers all of the plugin functionality into
    the application, no further configuration is needed.
    
    The plugin registry is the first class that aims for detecting new
    plugins and act as a source for the UI as well as acting as a collection
    for all known to the instance plugins.

---
+++ src/Inixe.Composable.App/Composition/PluginCompositionModule.cs
@@ -0,0 +1,70 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginCompositionModule.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition
+{
+    using System;
+    using Autofac;
+    using Inixe.Composable.App.Composition.PluginFramework;
+    using Inixe.Composable.UI.Core;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Loads and registers the WPF related classes.
+    /// </summary>
+    /// <seealso cref="Autofac.Module" />
+    internal class PluginCompositionModule : Module
+    {
+        /// <summary>
+        /// Override to add registrations to the container.
+        /// </summary>
+        /// <param name="builder">The builder through which components can be
+        /// registered.</param>
+        /// <remarks>
+        /// Note that the ContainerBuilder parameter is unique to this module.
+        /// </remarks>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PluginRegistry>()
+                .SingleInstance();
+
+            builder.RegisterType<CommandFactoryRegistry>()
+                .SingleInstance();
+
+            builder.RegisterType<RoutingCommandFactory>()
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<AsyncDispatcher>()
+                .SingleInstance()
+                .AsImplementedInterfaces();
+
+            builder.Register(this.CreateFilePluginSource)
+                .AsImplementedInterfaces();
+
+            builder.RegisterType<PluginInstance>();
+
+            builder.RegisterDecorator<PluginViewModelLocatorDecorator, IViewModelLocator>();
+            builder.RegisterComposite<CompositePluginSource, IPluginSource>();
+
+            base.Load(builder);
+        }
+
+        private IPluginSource CreateFilePluginSource(IComponentContext context)
+        {
+            try
+            {
+                var config = context.Resolve<IConfiguration>();
+                var pluginPath = config.GetValue(Constants.LocalPluginsDirectory, Constants.DefaultPluginDirectory);
+
+                return FileSystemPluginSource.Create(pluginPath);
+            }
+            catch (ArgumentException)
+            {
+                return new NullPluginSource();
+            }
+        }
+    }
+}
---
+++ src/Inixe.Composable.App/Composition/PluginFramework/PluginRegistry.cs
@@ -0,0 +1,124 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginRegistry.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition.PluginFramework
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+    using System.Linq;
+
+    /// <summary>
+    /// Instance collection.
+    /// </summary>
+    /// <seealso cref="System.Collections.ObjectModel.KeyedCollection{TKey, TValue}" />
+    public class PluginRegistry : KeyedCollection<Guid, PluginInstance>, INotifyCollectionChanged, INotifyPropertyChanged, IDisposable
+    {
+        /// <summary>
+        /// Occurs when the collection changes.
+        /// </summary>
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            foreach (var item in this.ToArray())
+            {
+                item.Dispose();
+            }
+
+            this.ClearItems();
+        }
+
+        /// <summary>
+        /// Called when the collection is changed.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <param name="action">The action.</param>
+        /// <exception cref="NotSupportedException">Could not notify action</exception>
+        protected virtual void OnCollectionChanged(PluginInstance instance, NotifyCollectionChangedAction action)
+        {
+            NotifyCollectionChangedEventArgs args;
+
+            switch (action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Remove:
+                    args = new NotifyCollectionChangedEventArgs(action, instance);
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    args = new NotifyCollectionChangedEventArgs(action);
+                    break;
+                default:
+                    throw new NotSupportedException("Could not notify action");
+            }
+
+            this.CollectionChanged?.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// Called when a property has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property.</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// When implemented in a derived class, extracts the key from the specified element.
+        /// </summary>
+        /// <param name="item">The element from which to extract the key.</param>
+        /// <returns>
+        /// The key for the specified element.
+        /// </returns>
+        protected override Guid GetKeyForItem(PluginInstance item)
+        {
+            return item.Manifest.Id;
+        }
+
+        /// <summary>
+        /// Inserts an element into the <see cref="T:System.Collections.ObjectModel.KeyedCollection`2" /> at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which <paramref name="item" /> should be inserted.</param>
+        /// <param name="item">The object to insert.</param>
+        protected override void InsertItem(int index, PluginInstance item)
+        {
+            base.InsertItem(index, item);
+            this.OnPropertyChanged(nameof(this.Count));
+            this.OnCollectionChanged(item, NotifyCollectionChangedAction.Add);
+        }
+
+        /// <summary>
+        /// Removes the element at the specified index of the <see cref="T:System.Collections.ObjectModel.KeyedCollection`2" />.
+        /// </summary>
+        /// <param name="index">The index of the element to remove.</param>
+        protected override void RemoveItem(int index)
+        {
+            var instance = this[index];
+            base.RemoveItem(index);
+
+            this.OnPropertyChanged(nameof(this.Count));
+            this.OnCollectionChanged(instance, NotifyCollectionChangedAction.Remove);
+        }
+
+        /// <summary>
+        /// Removes all elements from the <see cref="T:System.Collections.ObjectModel.KeyedCollection`2" />.
+        /// </summary>
+        protected override void ClearItems()
+        {
+            base.ClearItems();
+            this.OnPropertyChanged(nameof(this.Count));
+            this.OnCollectionChanged(null, NotifyCollectionChangedAction.Reset);
+        }
+    }
+}
