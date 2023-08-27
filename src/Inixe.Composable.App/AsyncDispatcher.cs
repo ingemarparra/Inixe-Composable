266358f6397ba80731ef4af9cdb3adfbc71606c9
ingemarparra <arsenico@inixe.com.mx>
2023-08-27 13:47:58 +0200

    Async Dispatcher implementation
    The Async Dispatcher implementation is required by Async base ICommands
    implementations in the core to carry out post task completed executions
    on threads other than the main thread.

---
+++ src/Inixe.Composable.App/AsyncDispatcher.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AsyncDispatcher.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using Inixe.Composable.UI.Core;
+
+    /// <summary>
+    /// WPF Dispatcher for Context Switching calls.
+    /// </summary>
+    /// <seealso cref="Inixe.Composable.UI.Core.IAsyncDispatcher" />
+    public sealed class AsyncDispatcher : IAsyncDispatcher
+    {
+        /// <summary>
+        /// Invokes the invokes an action on the UI Thread.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns>The operation task.</returns>
+        public async Task InvokeAsync(Action action)
+        {
+            await Application.Current.Dispatcher.BeginInvoke(action);
+        }
+    }
+}
