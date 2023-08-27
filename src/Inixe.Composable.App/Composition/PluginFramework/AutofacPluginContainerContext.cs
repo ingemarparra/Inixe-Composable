1712f078a36868ab054d1a06e9aec1634d72a537
ingemarparra <arsenico@inixe.com.mx>
2023-08-27 15:30:59 +0200

    Add base implementations for plugin framework
    The implementations for Container Context and Registry hide autofac from
    the consuming plugings and abstract the IoC.
    
    This can enable replacements of the IoC without having to perform
    modifications to the plugins.

---
+++ src/Inixe.Composable.App/Composition/PluginFramework/AutofacPluginContainerContext.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AutofacPluginContainerContext.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition.PluginFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using Autofac;
+    using Inixe.Composable.UI.Core;
+
+    /// <summary>
+    /// Autofac implementation of <see cref="IPluginContainerContext"/>.
+    /// </summary>
+    /// <seealso cref="Inixe.Composable.UI.Core.IPluginContainerContext" />
+    internal class AutofacPluginContainerContext : IPluginContainerContext, IViewModelLocator
+    {
+        private readonly ILifetimeScope componentContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacPluginContainerContext"/> class.
+        /// </summary>
+        /// <param name="componentContext">The component context.</param>
+        /// <exception cref="ArgumentNullException">componentContext.</exception>
+        internal AutofacPluginContainerContext(ILifetimeScope componentContext)
+        {
+            this.componentContext = componentContext ?? throw new ArgumentNullException(nameof(componentContext));
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerContext CreateScope(string name)
+        {
+            return new AutofacPluginContainerContext(this.componentContext.BeginLifetimeScope(name));
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.componentContext.Dispose();
+        }
+
+        /// <inheritdoc/>
+        public object ResolveService(Type type)
+        {
+            return this.componentContext.Resolve(type);
+        }
+
+        /// <inheritdoc/>
+        public T ResolveService<T>()
+        {
+            return this.componentContext.Resolve<T>();
+        }
+
+        /// <inheritdoc/>
+        public IList<T> ResolveServices<T>()
+        {
+            return this.componentContext.Resolve<IList<T>>();
+        }
+
+        /// <inheritdoc/>
+        object IViewModelLocator.GetViewModel(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Invalid View Model name", nameof(name));
+            }
+
+            return this.componentContext.ResolveNamed<object>(name);
+        }
+
+        /// <inheritdoc/>
+        bool IViewModelLocator.IsRegistered(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Invalid View Model name", nameof(name));
+            }
+
+            return this.componentContext.IsRegisteredWithName<object>(name);
+        }
+    }
+}
---
+++ src/Inixe.Composable.App/Composition/PluginFramework/AutofacPluginContainerRegistry.cs
@@ -0,0 +1,182 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AutofacPluginContainerRegistry.cs" company="Inixe S.A.">
+// Copyright All Rights reserved. Inixe S.A. 2023
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Inixe.Composable.App.Composition.PluginFramework
+{
+    using System;
+    using Autofac;
+    using Autofac.Builder;
+    using Inixe.Composable.UI.Core;
+
+    /// <summary>
+    /// Autofac implementation of <see cref="IPluginContainerRegistry"/>.
+    /// </summary>
+    /// <seealso cref="Inixe.Composable.UI.Core.IPluginContainerRegistry" />
+    internal class AutofacPluginContainerRegistry : IPluginContainerRegistry
+    {
+        private readonly ContainerBuilder containerBuilder;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutofacPluginContainerRegistry"/> class.
+        /// </summary>
+        /// <param name="containerBuilder">The container builder.</param>
+        /// <exception cref="ArgumentNullException">containerBuilder.</exception>
+        public AutofacPluginContainerRegistry(ContainerBuilder containerBuilder)
+        {
+            this.containerBuilder = containerBuilder ?? throw new ArgumentNullException(nameof(containerBuilder));
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterDecorator<T, TService>(Func<IPluginContainerContext, T> factory, ContainerLifeCycle containerLifeCycle)
+            where T : TService
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterFactory<T, TService>(Func<IPluginContainerContext, T> factory, ContainerLifeCycle containerLifeCycle)
+            where T : TService
+        {
+            ArgumentNullException.ThrowIfNull(factory, nameof(factory));
+
+            var registration = this.containerBuilder.Register<TService>(ctx =>
+            {
+                var scopeProvider = ctx.Resolve<ILifetimeScope>();
+                var pluginContext = new AutofacPluginContainerContext(scopeProvider);
+                return factory(pluginContext);
+            });
+
+            ConfigureRegistration(containerLifeCycle, registration);
+
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterFactory<T>(Func<IPluginContainerContext, T> factory, ContainerLifeCycle containerLifeCycle)
+        {
+            ArgumentNullException.ThrowIfNull(factory, nameof(factory));
+
+            var registration = this.containerBuilder.Register(ctx =>
+            {
+                var scopeProvider = ctx.Resolve<ILifetimeScope>();
+                var pluginContext = new AutofacPluginContainerContext(scopeProvider);
+                return factory(pluginContext);
+            });
+
+            ConfigureRegistration(containerLifeCycle, registration);
+
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterInstance<T>(T instance)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(instance, nameof(instance));
+            return this.RegisterInstance<T, T>(instance);
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterInstance<T, TService>(T instance)
+            where T : class, TService
+        {
+            ArgumentNullException.ThrowIfNull(instance, nameof(instance));
+
+            IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> registration = this.containerBuilder.RegisterInstance<T>(instance);
+            if (typeof(T) != typeof(TService))
+            {
+                registration.As<TService>();
+            }
+
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType<T>()
+        {
+            return this.RegisterType<T>(ContainerLifeCycle.Transient);
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType<T>(ContainerLifeCycle containerLifeCycle)
+        {
+            return this.RegisterType(typeof(T), typeof(T), containerLifeCycle);
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType<T, TService>()
+        {
+            return this.RegisterType<T, TService>(ContainerLifeCycle.Transient);
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType<T, TService>(ContainerLifeCycle containerLifeCycle)
+        {
+            return this.RegisterType(typeof(T), typeof(TService), containerLifeCycle);
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType(Type implementation, Type service, ContainerLifeCycle containerLifeCycle)
+        {
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+            ArgumentNullException.ThrowIfNull(service, nameof(service));
+
+            var registration = this.containerBuilder.RegisterType(implementation);
+            if (!implementation.Equals(service))
+            {
+                registration.As(service);
+            }
+
+            ConfigureRegistration(containerLifeCycle, registration);
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public IPluginContainerRegistry RegisterType(Type implementation, ContainerLifeCycle containerLifeCycle)
+        {
+            ArgumentNullException.ThrowIfNull(implementation, nameof(implementation));
+            return this.RegisterType(implementation, implementation, containerLifeCycle);
+        }
+
+        private static void ConfigureRegistration<T>(ContainerLifeCycle containerLifeCycle, IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> registration)
+        {
+            if (!containerLifeCycle.HasFlag(ContainerLifeCycle.Scoped) && containerLifeCycle.HasFlag(ContainerLifeCycle.Singleton))
+            {
+                registration = registration.SingleInstance();
+            }
+
+            if (containerLifeCycle.HasFlag(ContainerLifeCycle.Scoped) && !containerLifeCycle.HasFlag(ContainerLifeCycle.Singleton))
+            {
+                registration = registration.OwnedByLifetimeScope()
+                    .InstancePerLifetimeScope();
+            }
+
+            if (containerLifeCycle.HasFlag(ContainerLifeCycle.AutoActivate))
+            {
+                registration.AutoActivate();
+            }
+        }
+
+        private static void ConfigureRegistration(ContainerLifeCycle containerLifeCycle, IRegistrationBuilder<object, ConcreteReflectionActivatorData, SingleRegistrationStyle> registration)
+        {
+            if (!containerLifeCycle.HasFlag(ContainerLifeCycle.Scoped) && containerLifeCycle.HasFlag(ContainerLifeCycle.Singleton))
+            {
+                registration = registration.SingleInstance();
+            }
+
+            if (containerLifeCycle.HasFlag(ContainerLifeCycle.Scoped) && !containerLifeCycle.HasFlag(ContainerLifeCycle.Singleton))
+            {
+                registration = registration.OwnedByLifetimeScope()
+                    .InstancePerLifetimeScope();
+            }
+
+            if (containerLifeCycle.HasFlag(ContainerLifeCycle.AutoActivate))
+            {
+                registration.AutoActivate();
+            }
+        }
+    }
+}
